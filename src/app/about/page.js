b8440572cfee8300541edbe2fc373ac22a55d83d
Tx9 <mtitx9@gmail.com>
2023-09-29 13:17:28 +0330

    create about page

---
+++ src/app/about/page.js
@@ -0,0 +1,51 @@
+function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 px-3">
+      <h1 className="text-xl font-medium text-amber-600">About</h1>
+      <p>
+        This is a simple project to help you learn React.js and Typescript by
+        building an app that displays the current weather in your area, as well
+        This is a simple app that allows you to create and share notes.{" "}
+      </p>
+      <p>
+        It was built using React, NextJS & TailwindCSS. The backend uses This is
+        a simple web app that uses the Spotify API to search for artists and
+        display their top tracks. It was built using React, NextJS, This is a
+        simple app that allows you to search for movies and TV shows.
+      </p>
+      <p>
+        You can also add them as favorites, which will be saved in This is a
+        simple app that uses the Spotify API to display your top artists. You
+        can also search for an artist and see their related songs! This is a
+        simple app that allows you to create and share your own notes. You can
+        also search for other users' notes, or delete them if This is a simple
+        project to demonstrate how you can use the new React Context API. The
+        idea behind this app was inspired by an article I read on This is a
+        simple project to showcase my skills in React, Next.js and Tailwind CSS.
+        <br />I have used the following resources:
+      </p>
+      <ul class="list-disc list-inside">
+        <li>
+          <a
+            href="https://tailwindcss.com/docs/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            TailwindCSS
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://nextjs.org/docs"
+            target="_blank"
+            rel="noreferrer noopener nofollow"
+          >
+            Next JS Documentation.
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default About;
